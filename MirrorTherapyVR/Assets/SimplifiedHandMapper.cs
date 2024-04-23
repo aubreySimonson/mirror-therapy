d1a2a5959c08239691abadc8a788c6aacb107a90
Aubrey Simonson <followspotfour@gmail.com>
2024-04-23 15:15:33 -0400

    whatever changes I left in here?

---
+++ MirrorTherapyVR/Assets/SimplifiedHandMapper.cs
@@ -0,0 +1,251 @@
+//PART OF MIRROR THERAPY VR
+//This is the main script-- it takes the bones of one hand and
+//animates a model of the other from it.
+//put this script on the same gameobject that the OVRHand script is on of the real hand.
+//Last edited January 2023
+//???-->followspotfour@gmail.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SimplifiedHandMapper : MonoBehaviour
+{
+
+    public GameObject realHand;//should be a gameobject with OVRSkeleton on it
+    public GameObject fakeHand;//should not have OVRSkeleton
+    public List<Transform> bonesToSkip;//use if there are more fake bones than real bones
+    OVRSkeleton skeleton;
+
+    //every transform in the fake hand
+    public List<Transform> fakeBones;
+    List<OVRBone> realBones;
+    private OVRBone realWrist;
+
+    public GameObject fakeHandPointPrefab;//the spheres that show us where the bones are.
+    public List<GameObject> fakeHandPoints;//points that match the position of all of the bones on the real hand
+
+    private int bonesCounter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      skeleton = realHand.GetComponent<OVRSkeleton>();
+
+      //make the fakeBones skeleton
+      fakeBones = new List<Transform>();
+      foreach(Transform bone in fakeHand.transform){
+        GetMoreBones(bone);
+      }
+
+      //make real Bones skeleton
+      realBones = new List<OVRBone>();
+      SortBones();
+      realWrist = realBones[0];//this is a guess-- you have no idea what index this joint is at.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector3 realWristPosition = realBones[0].Transform.position;
+        foreach(OVRBone bone in realBones){
+          //put a hand point at each bone position
+          fakeHandPoints[bonesCounter].transform.position = bone.Transform.position;
+          fakeHandPoints[bonesCounter].transform.rotation = bone.Transform.rotation;
+
+          fakeBones[bonesCounter].transform.position = fakeHandPoints[bonesCounter].transform.position;
+          fakeBones[bonesCounter].transform.rotation = fakeHandPoints[bonesCounter].transform.rotation;
+
+          bonesCounter++;
+        }
+    }//end update
+
+    public List<OVRBone> GetRealBones(){
+      return realBones;
+    }
+
+    //a functon which does the recursive loop of getting all fake bones
+    private void GetMoreBones(Transform bone){
+      if(!bonesToSkip.Contains(bone)){
+        fakeBones.Add(bone);
+
+        GameObject newFakeHandPoint = Instantiate(fakeHandPointPrefab);
+        fakeHandPoints.Add(newFakeHandPoint);
+
+        if(bone.transform.childCount!=0){
+          foreach(Transform smallerBone in bone){
+            GetMoreBones(smallerBone);
+          }
+        }//end if
+      }//end if
+    }//end get more bones
+
+    //looks at all of the bones in the skeleton and put just the ones in the left hand
+    //into realBones. The order is super important because it has to match the fake bones
+    //you might think: we can refactor this, and make it much more elegant-- you would be wrong.
+    //OVR is sort of weird and fussy about bones?
+    private void SortBones(){
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_WristRoot)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_ForearmStub)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Index1)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Index2)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Index3)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Middle1)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Middle2)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Middle3)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Pinky0)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Pinky1)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Pinky2)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Pinky3)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Ring1)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Ring2)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Ring3)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Thumb0)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Thumb1)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Thumb2)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_Thumb3)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+
+      foreach (OVRBone bone in skeleton.Bones)
+      {
+        if (bone.Id == OVRSkeleton.BoneId.Hand_ThumbTip)
+        {
+          realBones.Add(bone);
+        }//end if
+      }//end foreach
+    }//end sortbones
+}
